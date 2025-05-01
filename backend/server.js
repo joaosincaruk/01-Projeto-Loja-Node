b92ff0c3e4909f9fef45a7505167b8eeefda2740
joaosincaruk <vieira.joaovic@gmail.com>
2025-05-01 16:04:33 -0300

    Inserção do Banco de dados, conexão com mongoDB e inserção de metodo try catch

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Servidor pronto!")
+
+
+app.get("/products", (req, res) => {
+    
 })
+
+console.log(process.env.MONGO_URI) 
+
 
 app.listen(5000, () => {
     console.log("Servidor iniciado com sucesso em http://localhost:5000")
 })
+
+// vQoOuwrB9O8ExzrD
