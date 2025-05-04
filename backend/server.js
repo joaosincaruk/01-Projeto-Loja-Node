6eb0c9e523e17c6fc7fdd3275227da08b7a98446
joaosincaruk <vieira.joaovic@gmail.com>
2025-05-04 20:22:52 -0300

    atualização server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -8,11 +9,21 @@ const app = express();
 
 
 
-app.post("/products", (req, res) => {
-    
+app.post("/products", async (req, res) => {
+    const Product = req.body
+    if (!Product.nome || !Product.preço || !Product.image){
+        return res.status(400).json({ success:false, message: "Por favor preencha todos os campos" })
+    }
+    const newProduct = new Product(product)
+ try{
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct})
+ }catch(error){
+    console.error("Erro na criação do produto")
+    res.status(500)
+ }
 })
 
-console.log(process.env.MONGO_URI) 
 
 
 app.listen(5000, () => {
